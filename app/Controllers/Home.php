5d3b4cebf6205326782f8cefe1e0383ae1c96def
AlanDavid-007 <alanpereiradavid@gmail.com>
2023-01-22 12:22:46 -0300

    Fragmentação da home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
         public function index()
     {
+        // Funtion cadastro, atualizar e excluir comentario
         helper('url');
         return view('Home');
     }
